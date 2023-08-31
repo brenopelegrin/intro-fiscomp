219fc84baf0f143ba67e628fccfb7767a294280b
brenopelegrin <breno.pelegrin@usp.br>
2023-08-31 00:35:48 -0300

    Minor changes to tarefa-5

--- projeto-1/tarefa-5/tarefa-5-13687303-simples.f
+++ projeto-1/tarefa-5/tarefa-5-13687303-simples.f
@@ -14,7 +14,7 @@ c     Aluno: Breno Henrique Pelegrin da Silva (13687303)
          else if (x .gt. 1.0d0) then
 c     Como a série só converge para x entre 0 e 2, podemos fazer
 c     -ln(1/x) = ln(x), já que 1/x < 1 quando x > 1
-            aLn = -1*fLnSerie(1/x)
+            aLn = -1.0e0*fLnSerie(1.0e0/x)
          end if
 
          diff = abs(aLn - aLnFortran)
