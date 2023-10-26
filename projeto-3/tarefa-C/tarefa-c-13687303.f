758e388463cb975168c7a8cd5098fb9586686cd3
brenopelegrin <breno.pelegrin@usp.br>
2023-10-25 23:32:55 -0300

    edit tarefa-C, add relatorio preview

--- projeto-3/tarefa-C/tarefa-c-13687303.f
+++ projeto-3/tarefa-C/tarefa-c-13687303.f
@@ -2,12 +2,35 @@
          implicit real*8 (a-h, o-z)
          x0 = -10.0d0
          e = 1e-6
+
          write(*,*) 'i,', 'r1,', 'r2,', 'r3'
          do i=1, 7, 1
             r1n = xnewton(x0, i, e)
             r2n = xnewton(0.0d0, i, e)
             r3n = xnewton(10.0d0, i, e)
             write(*,*) i-1, r1n, r2n, r3n
+         end do
+         write(*,*) '------'
+         n=100
+         h = real((b-a))/n
+         a = -10.0d0
+         b = 10.0d0
+         do i=1, n-1, 1
+            xold = a + i*h
+            xnew = xold + h
+            if(f(xnew)*f(xold) .le. 0.0d0) then
+               write(*,*) 'hi'
+ 99            if(abs(f(xold)) - abs(f(xnew)) .gt. e) then
+                  c = (xnew+xold)/2.0d0
+                  if(f(xnew)*f(xold) .gt. 0.0d0) then
+                     xnew = c
+                  else
+                     xold = c
+                  end if
+                  write(*,*) c
+                  goto 99
+               end if
+            end if
          end do
       end program
 
--- projeto-3/test.f
+++
@@ -1,5 +0,0 @@
-      program Test
-         dimension h(5)
-         h = [1,2,3,4,5]
-         write(*,*) h
-      end program Test
